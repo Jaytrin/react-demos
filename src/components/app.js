fd00f91f71c605d6b4156a1873d5d874bdc604e4
Jaytrin <jaytrin.apinchapong@gmail.com>
2018-09-14 11:21:48 -0700

    push to get clean master branch

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
+import Greeting from './greeting';
 
 const App = () => (
     <div>
         <div className="app">
+            <Greeting name="Tony Stark" favColor = "red"/>
+            <Greeting name ="Steve Rogers" favColor = "blue"/>
             <img src={logo} className="logo rotate"/>
             <h1>Welcome to React</h1>
         </div>
